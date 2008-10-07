6ad484eec18c11eef43cea863025dcc7342a3590
Will Gray <w.gray@vanderbilt.edu>
2008-10-07 10:26:52 -0500

    Wrapper for doing distance permutations.

---
+++ R/distancep.R
@@ -0,0 +1,9 @@
+distancep <- function(scores, group, group.1=NA, group.2=NA, verbose=TRUE, permutations=10000) {
+    result <- distance(scores, group, group.1, group.2, verbose)
+    cnt <- NA
+    for (i in 1:permutations) {
+        cnt[i] <- distance(sample(scores), group, group.1, group.2, verbose=FALSE)$accuracy[1]
+    }
+    result$distancep <- sum(cnt > result$accuracy[1]) / permutations
+    return(result)
+}
