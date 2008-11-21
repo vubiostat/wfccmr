97036f1355290f3feda90bdeb65e8023b075c5e8
Will Gray <w.gray@vanderbilt.edu>
2008-11-21 14:58:54 -0600

    Formatting.

--- R/generateSTDFunc.R
+++ R/generateSTDFunc.R
@@ -1,69 +1,55 @@
-generateSTDFunc <- function(availableCriteria, criteria, managertype){
-	func <- ""
-	pattern <- "^prob.([^ .])"
-	values <- c()
-	numScr <- 0
-	if(managertype == "TopN"){
-		numScr <- 1
-	}
+generateSTDFunc <- function(criteria, availableCriteria) {
+    func <- ""
+    pattern <- "^prob.([^ .])"
+    values <- c()
+    numScr <- 0
 
+    for (scr in criteria) {
+        scr <- strsplit(scr, split=" ", fixed=TRUE)
+        
+        switch(scr[[1]][1],
+            info={}, fdrPass={}, numPass={},
+            prob.pt.fdr=, prob.pt.fam.fdr={
+                if (length(grep("tvalue.std", values, fixed=TRUE, value=TRUE)) == 0)
+                    values <- c(values, "tvalue.std")
+                numScr <- numScr + 1
+            },
+            prob.c=, prob.c.fdr={
+                if (length(grep("czvalue", availableCriteria)) > 0) {
+                    if (length(grep("czvalue", values, fixed=TRUE, value=TRUE)) == 0)
+                        values <- c(values, "czvalue.std")
+                } else if (length(grep("cvalue", availableCriteria)) > 0) {
+                    if (length(grep("cvalue", values, fixed=TRUE, value=TRUE)) == 0)
+                        values <- c(values, "cvalue.std")
+                }
+                numScr <- numScr + 1
+            },
+            {
+                if (length(grep(pattern, scr[[1]][1])) > 0) {
+                    tmpChar <- sub("prob.", "", scr[[1]][1])
+                    tmp <- paste(tmpChar, "value.std", sep="")
 
-	for(i in 1:length(criteria)){
-		scr <- strsplit(criteria[i], split = " ", fixed = TRUE)
-		
-		switch(scr[[1]][1],
-			fdrpass=, numpass={},
-			prob.pt.fdr=, prob.pt.fam.fdr={
-				if(length(grep("tvalue.std", values, fixed = TRUE, value = TRUE)) == 0){
-					values <- c(values, "tvalue.std")
-				}
-				numScr <- numScr + 1
-			},
-			info={
-				if(managertype == "TopN"){
-					numScr <- numScr + 1
-				}
-			},
-			prob.c=, prob.c.fdr={
-				if(length(grep("czvalue", availableCriteria)) > 0){
-					if(length(grep("czvalue", values, fixed = TRUE, value = TRUE)) == 0){
-						values <- c(values, "czvalue.std")
-					}
-				}else if(length(grep("cvalue", availableCriteria)) > 0){
-					if(length(grep("cvalue", values, fixed = TRUE, value = TRUE)) == 0){
-						values <- c(values, "cvalue.std")
-					}
-				}
-				numScr <- numScr + 1
-			},
-			{
-				if(length(grep(pattern, scr[[1]][1])) > 0){
-					tmpChar <- sub("prob.","",scr[[1]][1])
-					tmp <- paste(tmpChar, "value.std", sep="")
+                    if (length(grep(paste(tmpChar, "value", sep=""), availableCriteria)) > 0) {
+                        if (length(grep(tmp, values, fixed=TRUE, value=TRUE)) == 0)
+                            values <- c(values, tmp)
+                    }
+                } else {
+                    tmp <- paste(scr[[1]][1], ".std", sep="")
+                    if (length(grep(tmp, values, fixed=TRUE, value=TRUE)) == 0)
+                        values <- c(values, tmp)
+                }
+                numScr <- numScr + 1
+            }
+        )
+    }
+    
+    if (numScr > 0) {
+        valueString <- ""
+        func <- paste("(numPass / ", numScr, ") * (",
+            if ("info" %in% availableCriteria) { "1 - info ) * ("},
+            paste(values, collapse=" + "),
+        ")", sep="")
+    }
 
-					if(length(grep(paste(tmpChar,"value", sep=""), availableCriteria)) > 0){
-						if(length(grep(tmp, values, fixed = TRUE, value = TRUE)) == 0){
-							values <- c(values, tmp)
-						}
-					}
-				}else{
-					tmp <- paste(scr[[1]][1],".std",sep="")
-					if(length(grep(tmp, values, fixed = TRUE, value = TRUE)) == 0){
-						values <- c(values, tmp)
-					}
-				}
-				numScr <- numScr + 1
-			}
-		)
-	}
-	
-	if(numScr > 0){
-		valueString <- ""
-		func <- paste("( numPass / ",numScr," ) * ( ",
-		if("info" %in% availableCriteria){"1 - info ) * ( "},
-		paste(values, collapse = " + "),
-		" )",sep="")
-	}
-
-	func
+    return(func)
 }
--- R/topnpanel.R
+++ R/topnpanel.R
@@ -1,25 +1,16 @@
-topnpanel <- function(final){
-	require(rpanel)
-	Draw <- function(panel) {
-		with(panel, {
-			plot(final)
-			abline(v=x, lwd=2, col=3)
-			text(x = 22, y = 25, labels = paste(names(final[[1]]$training$accuracy[1]),": ",round(final[[x]]$training$accuracy[[1]]*100, digits = 1)))
-			text(x = 22, y = 20, labels = paste(names(final[[1]]$training$accuracy[2]),": ",round(final[[x]]$training$accuracy[[2]]*100, digits = 1)))
-			text(x = 22, y = 15, labels = paste(names(final[[1]]$training$accuracy[3]),": ",round(final[[x]]$training$accuracy[[3]]*100, digits = 1)))
-		})
-		panel
-	}
+topnpanel <- function(object){
+    Draw <- function(panel) {
+        with(panel, {
+            plot(object, show.legend=FALSE)
+            abline(v=x, lwd=2, col=3)
+            #text(x = 22, y = 25, labels = paste(names(object[[1]]$training$accuracy[1]),": ",round(object[[x]]$training$accuracy[[1]]*100, digits = 1)))
+            #text(x = 22, y = 20, labels = paste(names(object[[1]]$training$accuracy[2]),": ",round(object[[x]]$training$accuracy[[2]]*100, digits = 1)))
+            #text(x = 22, y = 15, labels = paste(names(object[[1]]$training$accuracy[3]),": ",round(object[[x]]$training$accuracy[[3]]*100, digits = 1)))
+        })
+        return(panel)
+    }
 
-	None <- function(panel) {
-		#Don't do anything!
-	}
-	class(final) <- c("topn", "list")
-	x <- 0
-	overall <- 0
-	group1 <- 0
-	group2 <- 0
-
-	panel <- rp.control("TopN")
-	rp.slider(panel, x, 1, 30, Draw, showvalue=TRUE, resolution=1)
+    x <- 0
+    panel <- rp.control("TopN")
+    rp.slider(panel, x, 1, length(object), Draw, showvalue=TRUE, resolution=1)
 }
