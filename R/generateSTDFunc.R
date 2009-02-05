1926cdee5d9dd62b4f2c86f78fd91cfaef38ea36
Will Gray <w.gray@vanderbilt.edu>
2009-02-05 15:49:16 -0600

    Debugging the generateSTDfunc code.

--- R/generateSTDFunc.R
+++ R/generateSTDFunc.R
@@ -1,13 +1,12 @@
-generateSTDFunc <- function(criteria, availableCriteria) {
+generateSTDfunc <- function(criteria, availableCriteria) {
     func <- ""
-    pattern <- "^prob.([^ .])"
+    pattern <- "^prob\\.([^ .])"
     values <- c()
     numScr <- 0
+    criteria <- strsplit(as.character(criteria), split=" ", fixed=TRUE)
 
     for (scr in criteria) {
-        scr <- strsplit(scr, split=" ", fixed=TRUE)
-        
-        switch(scr[[1]][1],
+        switch(scr[1],
             info={}, fdrPass={}, numPass={},
             prob.pt.fdr=, prob.pt.fam.fdr={
                 if (length(grep("tvalue.std", values, fixed=TRUE, value=TRUE)) == 0)
@@ -15,26 +14,26 @@ generateSTDFunc <- function(criteria, availableCriteria) {
                 numScr <- numScr + 1
             },
             prob.c=, prob.c.fdr={
-                if (length(grep("czvalue", availableCriteria)) > 0) {
-                    if (length(grep("czvalue", values, fixed=TRUE, value=TRUE)) == 0)
-                        values <- c(values, "czvalue.std")
-                } else if (length(grep("cvalue", availableCriteria)) > 0) {
-                    if (length(grep("cvalue", values, fixed=TRUE, value=TRUE)) == 0)
-                        values <- c(values, "cvalue.std")
+                if (length(grep("cz.value", availableCriteria)) > 0) {
+                    if (length(grep("cz.value", values, fixed=TRUE, value=TRUE)) == 0)
+                        values <- c(values, "cz.value.std")
+                } else if (length(grep("c.value", availableCriteria)) > 0) {
+                    if (length(grep("c.value", values, fixed=TRUE, value=TRUE)) == 0)
+                        values <- c(values, "c.value.std")
                 }
                 numScr <- numScr + 1
             },
             {
-                if (length(grep(pattern, scr[[1]][1])) > 0) {
-                    tmpChar <- sub("prob.", "", scr[[1]][1])
-                    tmp <- paste(tmpChar, "value.std", sep="")
+                if (length(grep(pattern, scr[1])) > 0) {
+                    tmpChar <- sub("prob.", "", scr[1])
+                    tmp <- paste(tmpChar, "value.std", sep=".")
 
-                    if (length(grep(paste(tmpChar, "value", sep=""), availableCriteria)) > 0) {
+                    if (length(grep(paste(tmpChar, "value", sep="."), availableCriteria)) > 0) {
                         if (length(grep(tmp, values, fixed=TRUE, value=TRUE)) == 0)
                             values <- c(values, tmp)
                     }
                 } else {
-                    tmp <- paste(scr[[1]][1], ".std", sep="")
+                    tmp <- paste(scr[1], "std", sep=".")
                     if (length(grep(tmp, values, fixed=TRUE, value=TRUE)) == 0)
                         values <- c(values, tmp)
                 }
@@ -42,14 +41,14 @@ generateSTDFunc <- function(criteria, availableCriteria) {
             }
         )
     }
-    
-    if (numScr > 0) {
-        valueString <- ""
-        func <- paste("(numPass / ", numScr, ") * (",
-            if ("info" %in% availableCriteria) { "1 - info ) * ("},
-            paste(values, collapse=" + "),
-        ")", sep="")
-    }
 
+    func <- paste(  if (numScr > 0) { paste("numPass / ", numScr, sep="") },
+                    if ("info" %in% availableCriteria) { "1 - info" },
+                    if (length(values) > 0) { paste(values, collapse=' + ') },
+                    sep=") * (")
+
+    if (nchar(func) > 0) {
+        func <- paste("(", func, ")", sep="")
+    }
     return(func)
 }
