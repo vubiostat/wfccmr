85ce4467276ca595afcd506491346750256f8da1
Will Gray <w.gray@vanderbilt.edu>
2009-02-20 15:06:45 -0600

    Check for model distance permutations > 0.
    Was gettings some weird results, NA, Inf, NaN, etc.

--- R/distancep.R
+++ R/distancep.R
@@ -1,9 +1,13 @@
 distancep <- function(scores, group, group.1=NA, group.2=NA, verbose=TRUE, permutations=1000) {
     result <- distance(scores, group, group.1, group.2, verbose)
-    cnt <- NA
-    for (i in 1:permutations) {
-        cnt[i] <- distance(sample(scores), group, group.1, group.2, verbose=FALSE)$accuracy[1]
+    if (!is.na(permutations) && permutations > 0) {
+        cnt <- NA
+        for (i in 1:permutations) {
+            cnt[i] <- distance(sample(scores), group, group.1, group.2, verbose=FALSE)$accuracy[1]
+        }
+        result$distancep <- sum(cnt > result$accuracy[1]) / permutations
+    } else {
+        result$distancep <- NA
     }
-    result$distancep <- sum(cnt > result$accuracy[1]) / permutations
     return(result)
 }
